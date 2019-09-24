938cdbb3e7e972c38c2f7ce460c2024cec850b83
Firetokin <1322395986@qq.com>
2019-09-24 12:43:16 +0800

    第0步第2次代码

--- wf.c
+++ wf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
-char str[26];
-int num[26] = {0}, totalnum = 0 , k = 0;
+char str[52];
+double num[52] = {0.0};
+int totalnum = 0 , k = 0;
 
 void hello(char one, FILE*fp ){
     char letter; 
@@ -19,24 +20,61 @@ void hello(char one, FILE*fp ){
     return ;
 }
 
+
+void insertionSort(){
+	int i , j ;
+	double temp = 0;
+	char c ;
+	
+	for(i = 0; i < 52; i ++){
+    	num[i] = num[i]/(1.0 * totalnum);
+	}
+	
+	for(i = 1; i < 52; i ++){
+		j = i;
+		temp = num[i];
+		c = str[i];
+		while(j > 0.0 && temp < num[j-1]){
+			num[j] = num[j-1];
+			str[j] = str[j-1];
+			j --;
+		}
+		num[j] = temp;
+		str[j] = c;
+	}
+	
+	for(i= 0; i < 52; i ++){
+		printf("%c : %.2lf\n",str[i],num[i]);
+	}
+} 
+
+
 int main(){
     FILE *fp;
- 
-    int i=0 ;
+	if((fp=fopen("englishtest.txt", "r"))==NULL){
+        printf("Cannot open the file!\n");
+        return -1;
+    }
+    int i=0 ,j = 0, k = 0;
     float pl = 0.0;
-    str[0]='a';
-    for (i=0; i<26; i++){
+   
+    for (i=0; i<52; i++){
         str[i] = 'a' + i;
-        hello(str[i], fp);
+        
+        if(j < 26){
+        	str[i] = 'A' + j;
+        	hello(str[i], fp);
+        	j ++;
+		}
+		else{
+			str[i] = 'G' + j;
+			hello(str[i], fp);
+			j ++;
+		}
         rewind(fp);
     }
-    for(i = 0; i < 26; i ++){
-    	pl = 1.0 * num[i]/totalnum;
-    	printf("%c:%lf\n",str[i],pl);
-	}
-    printf("totalnum = %d\n",totalnum);
+    insertionSort();
     fclose(fp);
-
     return 0;
 }
 
