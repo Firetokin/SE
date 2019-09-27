6fff754fc89303f09ffff720d3262481db615d65
Firetokin <1322395986@qq.com>
2019-09-27 10:53:08 +0800

    第0步第三次大修改，main参数使用

--- wf.c
+++ wf.c
@@ -1,80 +1,76 @@
 #include<stdio.h>
+#include<string.h>
+#define N 26
+char str[N];
+double num[N];
+int total = 0;
 
-char str[52];
-double num[52] = {0.0};
-int totalnum = 0 , k = 0;
-
-void hello(char one, FILE*fp ){
-    char letter; 
-    int  ret;
-    while(!feof(fp)){    //fefo():文件结束：返回非0值，文件未结束:返回0值,fscanf（可以从一个文件流中格式化读出数据，遇到空格或回车就停止）
-     	ret = fscanf(fp, "%c", &letter);
-        if(letter==one){
-        	num[k] ++;
-        	totalnum ++;
-		}        
-        if (ret == EOF)
-            break;
-    }
-	k++;
-    return ;
+void countTotal(FILE*fp){
+	char ret;
+	while(!feof(fp)){ //fefo():文件结束：返回非0值，文件未结束:返回0值
+		ret = fgetc(fp);
+		if(ret >= 'A' && ret <= 'Z'){
+			num[ret - 'A'] ++;
+			total ++;
+		}
+		else if(ret >= 'a' && ret <= 'z'){
+			num[ret - 'a'] ++;
+			total ++;
+		}
+		else if(ret == EOF){
+			break;
+		}
+	}
 }
 
-
 void insertionSort(){
-	int i , j ;
-	double temp = 0;
-	char c ;
-	
-	for(i = 0; i < 52; i ++){
-    	num[i] = num[i]/(1.0 * totalnum);
+	int i, j;
+	double temp;
+	char tempstr;
+	for(i = 0; i < N;i ++){
+		num[i] = num[i] / (1.0 * total);
 	}
-	
-	for(i = 1; i < 52; i ++){
+	for(i = 1; i < N; i ++){
 		j = i;
 		temp = num[i];
-		c = str[i];
-		while(j > 0.0 && temp < num[j-1]){
+		tempstr = str[i];
+		while(j > 0 && temp < num[j-1]){
 			num[j] = num[j-1];
-			str[j] = str[j-1];
+			str[j] = str[j-1];  
 			j --;
 		}
 		num[j] = temp;
-		str[j] = c;
+		str[j] = tempstr;
 	}
 	
-	for(i= 0; i < 52; i ++){
-		printf("%c : %.2lf\n",str[i],num[i]);
+	for(i = 0; i < N; i ++){
+		printf("%c : %.2lf\n",str[i], num[i]);
 	}
 } 
 
-
-int main(){
-    FILE *fp;
-	if((fp=fopen("englishtest.txt", "r"))==NULL){
-        printf("Cannot open the file!\n");
-        return -1;
-    }
-    int i=0 ,j = 0, k = 0;
-    float pl = 0.0;
-   
-    for (i=0; i<52; i++){
-        str[i] = 'a' + i;
-        
-        if(j < 26){
-        	str[i] = 'A' + j;
-        	hello(str[i], fp);
-        	j ++;
-		}
-		else{
-			str[i] = 'G' + j;
-			hello(str[i], fp);
-			j ++;
-		}
-        rewind(fp);
-    }
-    insertionSort();
-    fclose(fp);
-    return 0;
+int main(int argc, char *argv[]){  //argv[1]保存指令，argv[2]保存文件路径 
+	FILE*fp;
+	int i;
+	
+	if(strcmp(argv[1],"-c") == 0){
+		fp = fopen(argv[2],"r");	
+	}
+	else{
+		printf("该指令与本程序功能不匹配！\n");
+	}
+	
+	for(i = 0; i < N; i ++){
+		str[i] = 'a' + i;
+	}
+	
+	if(fp != NULL){
+		countTotal(fp);
+		insertionSort();
+	}
+	else{
+		printf("文件打开失败！\n");
+	}
+	//printf("total = %d\n",total); 
+	fclose(fp);
+	return 0;
 }
-
